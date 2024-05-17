517a97e3fe5c77985723295246f4ff13078138b5
Steve Rosam <srosam+quix-demos@gmail.com>
2024-05-17 11:06:58 +0000

    Edited 'main.py'

--- ohlc_to_feature_store/main.py
+++ ohlc_to_feature_store/main.py
@@ -3,8 +3,13 @@ import logging
 from src.main import run
 from src.utils import initialize_logger
 
-initialize_logger()
+import os
+def print_env_variables():
+    for key, value in os.environ.items():
+        print(f"{key}: {value}")
 
-logger = logging.getLogger()
-logger.info('Starting ohlc_to_feature_store service...')
-run()
+# initialize_logger()
+
+# logger = logging.getLogger()
+# logger.info('Starting ohlc_to_feature_store service...')
+# run()
