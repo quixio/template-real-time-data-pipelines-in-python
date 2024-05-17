4e2ffbcf69d1b992ddef9270198113b5d168a4ce
Steve Rosam <srosam+quix-demos@gmail.com>
2024-05-17 11:08:04 +0000

    Edited 'main.py'

--- ohlc_to_feature_store/main.py
+++ ohlc_to_feature_store/main.py
@@ -1,12 +1,12 @@
-import logging
-
-from src.main import run
-from src.utils import initialize_logger
-
 import os
 def print_env_variables():
     for key, value in os.environ.items():
         print(f"{key}: {value}")
+        
+# import logging
+
+# from src.main import run
+# from src.utils import initialize_logger
 
 # initialize_logger()
 
