9968aa383bd1ee475645731684fcf9fec8a85bd3
Steve Rosam <srosam+quix-demos@gmail.com>
2024-05-17 11:08:40 +0000

    Edited 'main.py'

--- ohlc_to_feature_store/src/main.py
+++ ohlc_to_feature_store/src/main.py
@@ -2,6 +2,11 @@ import os
 import logging
 from typing import Optional
 
+import os
+def print_env_variables():
+    for key, value in os.environ.items():
+        print(f"{key}: {value}")
+        
 # import redis
 
 from src.utils import initialize_logger, load_env_vars
